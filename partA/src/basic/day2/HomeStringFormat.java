d81358162be79ab277a572bbe5598f6ae2171e45
jelly <kkh971025@gmail.com>
2023-12-29 17:45:49 +0900

    day2 소스

--- partA/src/basic/day2/HomeStringFormat.java
+++ partA/src/basic/day2/HomeStringFormat.java
@@ -3,6 +3,10 @@ package basic.day2;
 public class HomeStringFormat {
     public static void main(String[] args){
         // String.format()에 대한 예제를 작성하고 실행도 시켜보세요.
+        // %s = 문자열, %d = 정수
+        String name = "길현";
+        int age = 27;
+        System.out.println(String.format("제 이름은 %s이고 나이는 %d입니다.", name, age));
     }
     
 }
