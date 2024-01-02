47451d3f6b1063fdb53e80442ac02374f32517f7
jelly <kkh971025@gmail.com>
2024-01-02 17:49:19 +0900

    숙제

---
+++ partA/src/basic/day3/HomeCharType.java
@@ -0,0 +1,21 @@
+package basic.day3;
+
+/*
+ * 문자열을 구성하는 문자의 종류 분석하기
+ * 1) 문자열을 임의로 저장합니다. 예시 : "Hello Java Hi Linux~~~"
+ * 
+ * 2) 1)번의 문자열을 구성하는 문자의 종류와 개수를 구하여 출력합니다.
+ *  변수명 : 대문자의 갯수 upperCount
+ *          소문자의 갯수 lowerCount
+ *          숫자의 갯수 numberCount
+ *          기호의 갯수 symbolCount
+ * 
+ * 출력예시 : 
+ * -------------------------------------------------
+ * 문자열 : Hello* Java Hi* Linux~~~
+ * 문자 종류 : 대문자 4개 소문자 12개 숫자 0개 기호 8개
+ */
+
+public class HomeCharType {
+    
+}
